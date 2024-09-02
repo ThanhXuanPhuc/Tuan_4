6504a7306f9ffb7afbbab678d28c99d8ec0c5443
ADMIN <ADMIN@DESKTOP-DMC6CNQ>
2024-09-02 22:07:40 +0700

    2

---
+++ Tuan-3-Tuan3/tuan3/LTNhapXuatTT/src/main/java/lab09/Demo/DemoWriteFile.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab09.Demo;
+
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author ADMIN
+ */
+public class DemoWriteFile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String str = "em la co gai hay nang tien\n Em co tuoi hay khong co tuoi";
+        try {
+            //tao luong
+            FileWriter fw = new FileWriter("baitho.txt");
+            //Ghi du lieu
+            fw.write(str);
+            //dong luong
+            fw.close();
+            System.out.println("Da ghi xong");
+            
+        } catch (Exception ex){
+            System.out.println("Loi xay ra:" + ex.toString());
+        }
+    }
+    
+}
